009de0d60bba8aa6b73bae494e9da7d73b886ff8
kdomanczyk <41953542+kdomanczyk@users.noreply.github.com>
2018-08-02 13:01:15 +0200

    Revert "Revert "1""
    This reverts commit 591acd313e3c7290d4385875496387afc5688e6b.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,12 +113,9 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <h2>Jak działają ostrza?</h2>
-                        <h3>Zobacz, jak to działa:</h3>
-                        <ol>
-                            <li>Przykładasz nożyk do nacinanego materiału</li>
-                            <li>Prąd przepływa przez nożyki.</li>
-                            <li>Odciągasz nożyk - prąd przestaje płynąć.</li>
-                        </ol>
+                        
+                        <img src="images/_MG_6066.png" alt="Jak dzialają ostrza?">
+                        
                      </div>
                 </div>
             </div>
